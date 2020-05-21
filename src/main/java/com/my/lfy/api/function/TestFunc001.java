17995d4f6446428172d31003a9330a8fe5fa8103
LFY <lifuyuan@swifthealth.cn>
2020-05-21 22:04:45 +0800

    add  Function

--- src/main/java/com/my/lfy/api/function/TestFunc001.java
+++ src/main/java/com/my/lfy/api/function/TestFunc001.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.BeanUtils;
 
 import java.io.Serializable;

b37da9feda12b072afe2ea416a5bc86ca68f06f6
LFY <lifuyuan@swifthealth.cn>
2020-06-04 16:50:58 +0800

    add  Redis

--- src/main/java/com/my/lfy/api/thread/service/ThreadPoolServiceImpl.java
+++ src/main/java/com/my/lfy/api/thread/service/ThreadPoolServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.*;
 

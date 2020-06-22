34e58b14d092128a8f4c273b8ed05a9278f61bd4
LFY <lifuyuan@swifthealth.cn>
2020-06-22 21:02:42 +0800

    update Transactional

--- src/main/java/com/my/lfy/api/algorithm/FibonacciSeries.java
+++ src/main/java/com/my/lfy/api/algorithm/FibonacciSeries.java
@@ -10,9 +10,68 @@ public class FibonacciSeries {
 
     public static void main(String[] args) {
 
+        //first
+        for (int i = 0; i < 39; i++) {
+            System.out.print(fibonacciSeries(i) + "\t");
+        }
+        System.out.println();
+
+        //second
+        fibonacciSeriesSecond(39);
+
+        System.out.println();
+
+        //third
+        fibonacciSeriesThird(39);
+
     }
 
-    public static void fibonacciSeries() {
+    public static int fibonacciSeries(int n) {
 
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fibonacciSeries(n - 1) + fibonacciSeries(n - 2);
     }
+
+    public static void fibonacciSeriesSecond(int n) {
+        int[] arr = new int[n];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 1) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            System.out.print(arr[i] + "\t");
+        }
+    }
+
+    public static void fibonacciSeriesThird(int n) {
+
+        int first = 1;
+        int second = 1;
+        int reslut = 0;
+
+        System.out.print(0 + "\t" + 1 + "\t" + 1 + "\t");
+
+        for (int i = 3; i < n; i++) {
+
+            reslut = first + second;
+
+            first = second;
+
+            second = reslut;
+            System.out.print(reslut + "\t");
+
+        }
+    }
+
 }
--- src/main/java/com/my/lfy/api/algorithm/PascalTriangle.java
+++ src/main/java/com/my/lfy/api/algorithm/PascalTriangle.java
@@ -10,9 +10,25 @@ public class PascalTriangle {
 
     public static void main(String[] args) {
 
+        pascalTriangle();
     }
 
     public static void pascalTriangle() {
 
+
+        int rows = 10;
+        for (int i = 0; i < rows; i++) {
+
+            int number = 1;
+            System.out.format("%" + (rows - i) * 2 + "s", "");
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.format("%4d", number);
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
     }
 }

c36229995fd17d6eb533e5fbc602f788ff06455f
LFY <lifuyuan@swifthealth.cn>
2020-07-24 14:16:52 +0800

    add Thread

---
+++ src/main/java/com/my/lfy/api/algorithm/GreatestCommonDivisor.java
@@ -0,0 +1,67 @@
+package com.my.lfy.api.algorithm;
+
+/**
+ * describe : 求两个数的最大公约数
+ *
+ * @author lfy
+ * @date 2020/7/24
+ **/
+public class GreatestCommonDivisor {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * 暴力法
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public static int getValue1(int a, int b) {
+
+
+        return 1;
+    }
+
+    /**
+     * 辗转相除法（欧几里得算法）
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public static int getValue2(int a, int b) {
+
+
+        return 1;
+    }
+
+    /**
+     * 更相减损法
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public static int getValue3(int a, int b) {
+
+
+        return 1;
+    }
+
+    /**
+     * 优化的更新减损法
+     *
+     * @param a int
+     * @param b int
+     * @return int
+     */
+    public static int getValue4(int a, int b) {
+
+
+        return 1;
+    }
+}
---
+++ src/main/java/com/my/lfy/api/algorithm/LeastCommonMultiple.java
@@ -0,0 +1,16 @@
+package com.my.lfy.api.algorithm;
+
+/**
+ * describe : 求两个数的最小公倍数
+ *
+ * @author lfy
+ * @date 2020/7/24
+ **/
+public class LeastCommonMultiple {
+
+    public static void main(String[] args) {
+
+        int big = Math.max(1, 2);
+        int small = Math.min(1, 2);
+    }
+}

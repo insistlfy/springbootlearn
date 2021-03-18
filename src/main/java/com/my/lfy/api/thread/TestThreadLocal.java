6aa3543fe394baf1ac3405676cb36e912814c2f7
LFY <lifuyuan@swifthealth.cn>
2021-03-18 15:13:07 +0800

    add TestThreadLocal.java

--- src/main/java/com/my/lfy/api/thread/TestThreadLocal.java
+++ src/main/java/com/my/lfy/api/thread/TestThreadLocal.java
@@ -1,5 +1,11 @@
 package com.my.lfy.api.thread;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * TestThreadLocal
  *
@@ -12,6 +18,9 @@ public class TestThreadLocal {
 
     public static void main(String[] args) {
 
+        /**
+         * Example one
+         */
         TestThreadLocal testThreadLocal = new TestThreadLocal();
 
         for (int i = 0; i < 5; i++) {
@@ -22,6 +31,12 @@ public class TestThreadLocal {
                 System.out.println(Thread.currentThread().getName() + ":" + context);
             }).start();
         }
+
+
+        /**
+         * Example two
+         */
+        new ClassA().process();
     }
 
 
@@ -32,6 +47,55 @@ public class TestThreadLocal {
     public void setContext(String context) {
         threadLocal.set(context);
     }
+}
+
+class ClassA {
+
+    public void process() {
+        User user = User.builder()
+                .name("James")
+                .sex("M")
+                .build();
+        UserContextHolder.holder.set(user);
+        new ClassB().process();
+    }
+
+}
+
+@Slf4j
+class ClassB {
+
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        log.info("ClassB拿到的用户信息，name={},sex={}。", user.getName(), user.getSex());
+        new ClassC().process();
+    }
+
+}
+
+@Slf4j
+class ClassC {
+
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        log.info("ClassC拿到的用户信息，name={},sex={}。", user.getName(), user.getSex());
+    }
+
+}
+
+class UserContextHolder {
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class User {
+
+    private String name;
+
+    private String sex;
 }
 
 

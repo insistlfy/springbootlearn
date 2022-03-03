6c2ca580e41a863460737f3311ffb9226f9a3fc2
lfy <lify@dfwytech.com>
2022-03-03 16:05:19 +0800

    update TestStopWatch

--- src/main/java/com/my/lfy/api/test/TestStopWatch.java
+++ src/main/java/com/my/lfy/api/test/TestStopWatch.java
@@ -4,7 +4,8 @@ import org.springframework.util.StopWatch;
 
 /**
  * @FileName: TestStopWatch
- * @Description: TODO
+ * @Description: 秒表
+ *               使用场景：在一个大任务下，可能有多个小的步骤任务，而我们需要知道各个步骤任务的用时情况
  * @Author: Lify
  * @CreateDate: 2022/1/18 18:41
  * @Version: 1.0
@@ -13,13 +14,13 @@ public class TestStopWatch {
 
     public static void main(String[] args) throws InterruptedException {
 
-        StopWatch stopWatch = new StopWatch();
+        StopWatch stopWatch = new StopWatch("StopWatch");
         for (int i = 0; i < 10; i++) {
             stopWatch.start("Task" + i);
             //Some Real Business Logic
-            Thread.sleep(1000);
+            Thread.sleep(500);
             stopWatch.stop();
         }
-        System.out.println(stopWatch.prettyPrint());;
+        System.out.println(stopWatch.prettyPrint());
     }
 }
